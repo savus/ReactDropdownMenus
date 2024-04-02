0ccac7f3bac5c09e41dfbd87709fd703b97eea99
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2024-04-02 14:15:30 -0600

    restart everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
-import { DropdownMenu } from "./Components/DropdownMenu";
-import { NavItem } from "./Components/NavItem";
-import { Navbar } from "./Components/Navbar";
 import "./css/index.css";
 
-import { CSSTransitions } from "react-transition-group";
-
 function App() {
-  return (
-    <>
-      <Navbar>
-        <NavItem icon="fa-solid fa-plus">
-          <></>
-        </NavItem>
-        <NavItem icon="fa-solid fa-bell">
-          <></>
-        </NavItem>
-        <NavItem icon="fa-brands fa-facebook-messenger">
-          <></>
-        </NavItem>
-
-        <NavItem icon="fa-solid fa-caret-down">
-          <DropdownMenu />
-        </NavItem>
-      </Navbar>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
