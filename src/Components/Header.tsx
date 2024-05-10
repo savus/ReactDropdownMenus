f4c23fd2fc29e711755fc3e2cecd42b626f69836
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2024-05-09 23:40:21 -0600

    build javascript logic to handle dropdown-menu functionality: dropdown-menu appears when link is clicked, link remains "focused" while dropdown is active, dropdown remains if clicked on, dropdown disappears when clicked outside of

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,14 +3,18 @@ import "../css/header.css";
 export function Header() {
   return (
     <div className="header">
-      <div className="dropdown">
-        <button className="link">Information</button>
+      <div className="dropdown" data-dropdown>
+        <button className="link" data-dropdown-button>
+          Information
+        </button>
         <div className="dropdown-menu">Dropdown Content</div>
       </div>
       <a href="#" className="link">
         Pricing
       </a>
-      <button className="link">Login</button>
+      <button className="link" data-dropdown-button>
+        Login
+      </button>
     </div>
   );
 }
