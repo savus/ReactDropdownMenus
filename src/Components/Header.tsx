023b6c78747c1c6cb88b46bfed6e2457f7e0abe2
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2024-05-15 16:34:25 -0600

    build own practice dropdown menu

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,14 +7,94 @@ export function Header() {
         <button className="link" data-dropdown-button>
           Information
         </button>
-        <div className="dropdown-menu">Dropdown Content</div>
+        <div className="dropdown-menu information-grid">
+          <div>
+            <div className="dropdown-heading">Free Tutorials</div>
+            <div className="dropdown-links">
+              <a href="#" className="link">
+                All
+              </a>
+              <a href="#" className="link">
+                Latest
+              </a>
+              <a href="#" className="link">
+                Popular
+              </a>
+            </div>
+          </div>
+          <div>
+            <div className="dropdown-heading">Courses</div>
+            <div className="dropdown-links">
+              <a href="#" className="link">
+                Javascript
+              </a>
+              <a href="#" className="link">
+                Css
+              </a>
+              <a href="#" className="link">
+                React
+              </a>
+            </div>
+          </div>
+          <div>
+            <div className="dropdown-heading">Blog</div>
+            <div className="dropdown-links">
+              <a href="#" className="link">
+                All
+              </a>
+              <a href="#" className="link">
+                Latest
+              </a>
+              <a href="#" className="link">
+                Popular
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
       <a href="#" className="link">
         Pricing
       </a>
-      <button className="link" data-dropdown-button>
-        Login
-      </button>
+      <a href="#">
+        <div className="dropdown" data-dropdown>
+          <button className="link" data-dropdown-button>
+            Login
+          </button>
+          <div className="dropdown-menu">
+            <form action="#" className="login-form">
+              <label htmlFor="email">mail</label>
+              <input type="email" name="email" id="email" />
+              <label htmlFor="password">password</label>
+              <input type="password" name="password" id="password" />
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </div>
+      </a>
+      <a href="#">
+        <div className="practice-dropdown" data-practice-dropdown>
+          <button className="dropdown-button" data-practice-dropdown-button>
+            products
+          </button>
+          <div className="practice-dropdown-menu" data-practice-dropdown-menu>
+            <div className="products-header">Products</div>
+            <ul className="item-list">
+              <li>
+                <a href="#">stuff1</a>
+              </li>
+              <li>
+                <a href="#">stuff2</a>
+              </li>
+              <li>
+                <a href="#">stuff3</a>
+              </li>
+              <li>
+                <a href="#">stuff4</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </a>
     </div>
   );
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("click", (e) => {
     const target = e.target;
-    const isDropdownButton = target.matches("[data-dropdown-button]");
-    if (!isDropdownButton && target.closest("[data-dropdown]") != null) return;
+    const dataDropdownButton = "[data-dropdown-button]";
+    const dataDropdown = "[data-dropdown]";
+    const isDropdownButton = target.matches(dataDropdownButton);
 
-    let currentDropdown;
+    if (!isDropdownButton && target.closest(dataDropdown) != null) return;
+
+    let currentDropdownMenu;
+
     if (isDropdownButton) {
-      currentDropdown = target.closest("[data-dropdown]");
-      currentDropdown.classList.toggle("active");
+      currentDropdownMenu = target.closest(dataDropdown);
+      currentDropdownMenu.classList.toggle("active");
     }
 
-    document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
-      if (dropdown === currentDropdown) return;
+    document.querySelectorAll(`${dataDropdown}.active`).forEach((dropdown) => {
+      if (dropdown === currentDropdownMenu) return;
+
       dropdown.classList.remove("active");
     });
+
+    const dataPracticeDropdown = "[data-practice-dropdown]";
+    const dataPracticeDropdownButton = "[data-practice-dropdown-button]";
+    const isPracticeDropdownButton = target.matches(dataPracticeDropdownButton);
+
+    if (
+      !isPracticeDropdownButton &&
+      target.closest(dataPracticeDropdown) != null
+    )
+      return;
+
+    let currentPracticeDropdownMenu;
+
+    if (isPracticeDropdownButton) {
+      currentPracticeDropdownMenu = target.closest(dataPracticeDropdown);
+      currentPracticeDropdownMenu.classList.toggle("active");
+    }
+
+    document
+      .querySelectorAll(`${dataPracticeDropdown}.active`)
+      .forEach((dropdown) => {
+        if (dropdown === currentPracticeDropdownMenu) return;
+        dropdown.classList.remove("active");
+      });
   });
 });
